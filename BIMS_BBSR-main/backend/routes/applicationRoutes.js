c00a22bad2e8bf4e86f16c12bc7d6d17a8233217
Abhijit Nayak <abhijitnayak275@gmail.com>
2026-06-01 14:04:06 +0530

    Remove duplicate BIMS_BBSR-main folder

--- BIMS_BBSR-main/backend/routes/applicationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { submitApplication } from '../controllers/applicationController.js';
-
-const router = express.Router();
-
-router.post('/', submitApplication);
-
-export default router;
